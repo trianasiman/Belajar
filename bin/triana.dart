423cc171ef1694005c1ec8703fb7dae4bb66c987
triana s iman <trianasiman@gmail.com>
2025-08-28 20:30:47 +0900

    Upload Project Pertama

---
+++ bin/triana.dart
@@ -0,0 +1,5 @@
+import 'package:triana/triana.dart' as triana;
+
+void main(List<String> arguments) {
+  print('Hello triana: ${triana.calculate()}!');
+}
---
+++ test/triana_test.dart
@@ -0,0 +1,8 @@
+import 'package:triana/triana.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
