cbaaf833fdb6d9cb084026ac57356720797de460
triana s iman <trianasiman@gmail.com>
2025-09-21 21:37:10 +0900

    pertemuan 1 latihan_2

---
+++ bin/latihan_2.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+  // Menyimpan panjang dan lebar
+  double panjang = 10;
+  double lebar = 5;
+
+  // Rumus menghitung luas dan keliling
+  double luas = panjang * lebar;
+  double keliling = 2 * (panjang + lebar);
+
+  // Menampilkan hasil
+  print("Panjang = $panjang");
+  print("Lebar = $lebar");
+  print("Luas = $luas");
+  print("Keliling = $keliling");
+
+}
